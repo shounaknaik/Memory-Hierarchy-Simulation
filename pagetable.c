b21e2313aea72b3736aecf268eca56ca051d0d13
Parmesh Mathur <parmeshkmathur@gmail.com>
2021-04-17 17:16:00 +0530

    Page directory entries taken care of

--- pagetable.c
+++ pagetable.c
@@ -1,7 +1,18 @@
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include "pagetable.h"
 #include "mainmemory.h"
+
+//Temp Declaration
+typedef struct
+{
+    unsigned int pid;
+    page_table* p_table_addr;
+} pcb;
+pcb temp_pcb;
+main_memory* mm;
+//
 
 page_table* page_table_init()
 {
@@ -17,21 +28,39 @@ page_table* page_table_init()
 
 main_memory_block* get_address(unsigned int block_number)
 {
-    // TODO:
-    // Outermost level one page with 4 entries
+    // TODO: Outermost level one page with 4 entries
+    // DONE
+
+    page_table* outer = temp_pcb.p_table_addr;
+    unsigned int middle, inner;
+    unsigned int outerindex = block_number >> 14;
     // 10 addresses map to 00 and 01
+    if(outerindex==32) middle = outer->entry_table[0].pageframe.p_table_index;
+    else if(outerindex==33) middle = outer->entry_table[1].pageframe.p_table_index;
     // 7f addresses map to 10 and 11
-    // Each entry points to a page directory with 128 entries
+    else if(outerindex==254) middle = outer->entry_table[2].pageframe.p_table_index; //Never occurs
+    else if(outerindex==255) middle = outer->entry_table[3].pageframe.p_table_index;
+
+    else //INVALID entry
+    {
+        // printf("invalid entry\n");
+        return NULL;
+    }
+
     // Middle level
-    // Each entry points to a page table with 128 entries each
+    // Each entry points to a page directory with 128 entries each
+    unsigned int middleindex = (block_number >> 7)%128;
+    inner = mm->p_tables[middle]->entry_table[middleindex].pageframe.p_table_index;
     // Innermost level
     // Each entry in outermost level points to a block containing 128 entries each
+    unsigned int frameindex = block_number % 128;
+    main_memory_block* retval;
+    //Go to the table whose adress was found (inner), get the required entry (frameindex) and get frame number
+    unsigned int frame_address = mm->p_tables[inner]->entry_table[frameindex].pageframe.frame_num;
+    retval = mm->blocks[frame_address];
     // Each of these entries points to a block/frame, return this pointer.
 
-    // 9 bits for frame offset
-    // 7 bits page table numble
-    // 7 bits page directory number
-    // 9 bits (effectively 2) for outer directory number
+    return retval;
 }
 
 void page_table_free(page_table* p_table)
--- pagetable.h
+++ pagetable.h
@@ -4,17 +4,23 @@
 #define VALID 1
 #define INVALID 0
 
+typedef union pageframe
+{
+    unsigned int frame_num:16;
+    unsigned int p_table_index;
+}p_f_address;
+
 typedef struct
 {
     unsigned int page_number:23; //TODO: will be removed. page tables indexed by page number.
-    unsigned int frame_number:16;
+    p_f_address pageframe;
     unsigned int valid_bit:1;
     unsigned int shared_bit:1;
 } page_table_entry;
 
 typedef struct
 {
-    page_table_entry entry_table[64];
+    page_table_entry entry_table[128];
 } page_table;
 
 #endif
